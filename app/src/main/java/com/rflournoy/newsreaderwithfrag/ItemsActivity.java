eff91b9cea8237252c2b7bb9e194f6f0cb04dc88
Randall Flournoy <66640@coleman.local>
2016-02-17 19:20:42 -0800

    Initial commit

---
+++ app/src/main/java/com/rflournoy/newsreaderwithfrag/ItemsActivity.java
@@ -0,0 +1,14 @@
+package com.rflournoy.newsreaderwithfrag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ItemsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items);
+
+    }
+}
